4a260f29ed8f9f4eff6f462b02144ccf6d71d2c3
elizavetaKrainova <elizaveta.krainova@accenture.com>
2025-03-09 00:26:59 +0100

    added build

--- src/mixin.js
+++
@@ -1,7 +0,0 @@
-export function setToLocalStorage(field, value) {
-  localStorage.setItem(field, JSON.stringify(value));
-}
-
-export function getFromLocalStorage(field) {
-  return JSON.parse(localStorage.getItem(field));
-}
